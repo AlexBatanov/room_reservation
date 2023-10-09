384a33966302e04804372cdd16c5dc19f30e03c4
Alexandr <pokertrend@yandex.ru>
2023-10-09 21:31:07 +0400

    created schemaas

--- app/models/meeting_room.py
+++ app/models/meeting_room.py
@@ -5,7 +5,5 @@ from app.core.db import Base
 
 
 class MeetingRoom(Base):
-    # Имя переговорки должно быть не больше 100 символов,
-    # уникальным и непустым.
     name = Column(String(100), unique=True, nullable=False) 
     description = Column(Text)
