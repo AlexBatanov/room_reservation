0fb22a159d3edc454fdb7c9fa9f48ae0b2297af7
Alexandr Batanov <pokertrend@yandex.ru>
2023-10-12 13:40:02 +0400

    reservation and room are ready

--- app/api/endpoints/meeting_room.py
+++ app/api/endpoints/meeting_room.py
@@ -1,12 +1,16 @@
 # app/api/meeting_room.py
+from datetime import datetime
 from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy import and_, select
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.db import get_async_session
 # Вместо импортов 6 функций импортируйте объект meeting_room_crud.
 from app.crud.meeting_room import meeting_room_crud
+from app.crud.reservation import reservation_crud
 from app.models.meeting_room import MeetingRoom
+from app.models.reservation import Reservation
 from app.schemas.meeting_room import (
     MeetingRoomCreate, MeetingRoomDB, MeetingRoomUpdate
 )
@@ -80,3 +84,18 @@ async def remove_meeting_room(
     # Замените вызов функции на вызов метода.
     meeting_room = await meeting_room_crud.remove(meeting_room, session)
     return meeting_room
+
+
+@router.get(
+    '/{meeting_room_id}/reservations',
+    response_model=list[ReservationDB]
+)
+async def get_reservations_for_room(
+        meeting_room_id: int,
+        session: AsyncSession = Depends(get_async_session),
+):
+    await check_meeting_room_exists(meeting_room_id, session)
+    reservations = await reservation_crud.get_future_reservations_for_room(
+        room_id=meeting_room_id, session=session
+    )
+    return reservations
